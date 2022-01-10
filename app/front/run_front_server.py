e84fd1b7ef91b1599dd6b61379460275c6c3daf8
Iranti <Iranti222@gmail.com>
2022-01-10 12:21:18 +0300

    3rd version

--- app/front/run_front_server.py
+++ app/front/run_front_server.py
@@ -61,8 +61,8 @@ def predict_form():
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
+#<<<<<<< HEAD
     app.run(host='0.0.0.0', port=8181, debug=True)
-=======
+#=======
     app.run(host='0.0.0.0', port=8181, debug=True)
->>>>>>> origin/main
+#>>>>>>> origin/main
